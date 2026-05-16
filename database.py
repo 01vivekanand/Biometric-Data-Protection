23043c053af6bcf5fc40440f8d4019e5e3a34387
Vivekanand Pandey <01vivekanandpandey@gmail.com>
2026-05-16 16:51:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="mysql.railway.internal",
+        host="autorack.proxy.rlwy.net",
         user="root",
         password="ZXOoeSeihQrGuIPbYtoOWkLdnoPwOSWg",
         database="railway",
